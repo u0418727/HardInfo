dce71785f3e8bd583b06d89055ab1ad2344adc58
Uliana Buslaeva <135011745+u0418727@users.noreply.github.com>
2024-05-06 00:14:19 +0300

    Добавьте файлы проекта.

---
+++ HardInfo1/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Hardlnfo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.hardwarePart = new System.Windows.Forms.ToolStripComboBox();
+            this.hardwarePartInfo = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hardwarePart});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Padding = new System.Windows.Forms.Padding(5);
+            this.toolStrip1.Size = new System.Drawing.Size(1000, 38);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // hardwarePart
+            // 
+            this.hardwarePart.Items.AddRange(new object[] {
+            "Процессор",
+            "Видеокарта",
+            "Сокет",
+            "Батарея",
+            "Биос",
+            "Оперативная память",
+            "Кэш",
+            "USB",
+            "Диск",
+            "Логические диски",
+            "Клавиатура",
+            "Сеть",
+            "Пользователи",
+            "Динамическая информация"});
+            this.hardwarePart.Name = "hardwarePart";
+            this.hardwarePart.Size = new System.Drawing.Size(200, 30);
+            this.hardwarePart.SelectedIndexChanged += new System.EventHandler(this.hardwarePart_SelectedIndexChanged);
+            // 
+            // hardwarePartInfo
+            // 
+            this.hardwarePartInfo.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.hardwarePartInfo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.hardwarePartInfo.FullRowSelect = true;
+            this.hardwarePartInfo.GridLines = true;
+            this.hardwarePartInfo.HideSelection = false;
+            this.hardwarePartInfo.Location = new System.Drawing.Point(0, 38);
+            this.hardwarePartInfo.Margin = new System.Windows.Forms.Padding(4);
+            this.hardwarePartInfo.Name = "hardwarePartInfo";
+            this.hardwarePartInfo.Size = new System.Drawing.Size(1000, 609);
+            this.hardwarePartInfo.TabIndex = 2;
+            this.hardwarePartInfo.UseCompatibleStateImageBehavior = false;
+            this.hardwarePartInfo.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Название";
+            this.columnHeader1.Width = 200;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Значение";
+            this.columnHeader2.Width = 200;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Информация";
+            this.columnHeader3.Width = 200;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 647);
+            this.Controls.Add(this.hardwarePartInfo);
+            this.Controls.Add(this.toolStrip1);
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripComboBox hardwarePart;
+        private System.Windows.Forms.ListView hardwarePartInfo;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+    }
+}
+
---
+++ HardInfo1/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Management;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace Hardlnfo
+{
+    public partial class Form1 : Form
+    {
+        private PerformanceCounter cpuCounter;
+        private PerformanceCounter memUsageCounter;
+        private PerformanceCounter memFreeCounter;
+        private Dictionary<string, PerformanceCounter> diskFreeCounters;
+        private Dictionary<string, float> diskTotalSizes; // Для хранения общего размера каждого диска
+        private Timer timer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeCPUCounter();
+            InitializeMemoryCounters();
+            InitializeDiskCounters();
+            InitializeTimer();
+        }
+
+        private void InitializeCPUCounter()
+        {
+            cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total", true);
+        }
+
+        private void InitializeMemoryCounters()
+        {
+            // Используемая память
+            memUsageCounter = new PerformanceCounter("Memory", "Committed Bytes", null, true);
+            // Свободная память
+            memFreeCounter = new PerformanceCounter("Memory", "Available MBytes", null, true);
+        }
+
+        private void InitializeDiskCounters()
+        {
+            diskFreeCounters = new Dictionary<string, PerformanceCounter>();
+            diskTotalSizes = new Dictionary<string, float>();
+            var driveQuery = new ManagementObjectSearcher("SELECT * FROM Win32_LogicalDisk WHERE DriveType=3");
+
+            foreach (ManagementObject drive in driveQuery.Get())
+            {
+                var name = drive["Name"].ToString();
+                diskFreeCounters[name] = new PerformanceCounter("LogicalDisk", "Free Megabytes", name, true);
+
+                // Получение и сохранение общего размера диска
+                ulong totalSize = (ulong)drive["Size"] / (1024 * 1024); // Преобразование в мегабайты
+                diskTotalSizes[name] = totalSize;
+            }
+        }
+
+        private void InitializeTimer()
+        {
+            timer = new Timer();
+            timer.Interval = 1000;
+            timer.Tick += (sender, e) =>
+            {
+                hardwarePartInfo.Items.Clear();
+                UpdateCPUUsage();
+                UpdateMemoryUsage();
+                UpdateDiskUsage();
+            };
+        }
+
+        private void UpdateCPUUsage()
+        {
+            float cpuUsage = cpuCounter.NextValue();
+            ListViewItem item = new ListViewItem("Загрузка процессора");
+            item.SubItems.Add($"{cpuUsage:N2}%");
+            UpdateOrAddListViewItem(item, "CPU");
+        }
+
+        private void UpdateMemoryUsage()
+        {
+            float memUsage = memUsageCounter.NextValue() / (1024 * 1024 * 1024); // Преобразование в гигабайты
+            float memFree = memFreeCounter.NextValue();
+            ListViewItem itemUsage = new ListViewItem("Используемая память");
+            itemUsage.SubItems.Add($"{memUsage:N2} GB");
+            ListViewItem itemFree = new ListViewItem("Свободная память");
+            itemFree.SubItems.Add($"{memFree:N2} MB");
+
+            UpdateOrAddListViewItem(itemUsage, "MemoryUsed");
+            UpdateOrAddListViewItem(itemFree, "MemoryFree");
+        }
+
+        private void UpdateDiskUsage()
+        {
+            foreach (var disk in diskFreeCounters.Keys)
+            {
+                float freeSpace = diskFreeCounters[disk].NextValue(); // Свободное пространство в мегабайтах
+                float totalSize = diskTotalSizes[disk];
+                float usedSpace = totalSize - freeSpace; // Рассчитываем занятое пространство
+
+                // Создаем элемент для отображения информации о диске
+                ListViewItem item = new ListViewItem($"Диск {disk}");
+                item.SubItems.Add($"Занято: {usedSpace:N0} MB");
+                item.SubItems.Add($"Свободно: {freeSpace:N0} MB");
+                item.SubItems.Add($"Всего: {totalSize:N0} MB");
+
+                // Обновляем или добавляем элемент в ListView
+                UpdateOrAddListViewItem(item, $"Disk {disk}");
+            }
+        }
+
+        private void UpdateOrAddListViewItem(ListViewItem item, string key)
+        {
+            var existingItem = hardwarePartInfo.Items.Find(key, false);
+            if (existingItem.Length > 0)
+                hardwarePartInfo.Items[hardwarePartInfo.Items.IndexOf(existingItem[0])] = item;
+            else
+                hardwarePartInfo.Items.Add(item);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            hardwarePart.SelectedIndex = 0; // Выбор первого элемента в списке
+            UpdateCPUUsage();
+            UpdateMemoryUsage();
+            UpdateDiskUsage();
+        }
+        private void GetHardWareInfo(string key, ListView list)
+        {
+            list.Items.Clear();
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM " + key);
+
+            try
+            {
+                foreach (ManagementObject obj in searcher.Get())
+                {
+                    ListViewGroup listViewGroup;
+
+                    try
+                    {
+                        listViewGroup = list.Groups.Add(obj["Name"].ToString(), obj["Name"].ToString());
+                    }
+                    catch (Exception ex)
+                    {
+                        listViewGroup = list.Groups.Add(obj.ToString(), obj.ToString());
+                    }
+
+                    foreach (PropertyData data in obj.Properties)
+                    {
+                        ListViewItem item = new ListViewItem(listViewGroup);
+                        item.UseItemStyleForSubItems = false;
+                        item.BackColor = list.Items.Count % 2 == 0 ? Color.WhiteSmoke : Color.White;
+                        item.Text = data.Name;
+
+                        if (data.Value != null && !string.IsNullOrEmpty(data.Value.ToString()))
+                        {
+                            string displayValue = ConvertDataToString(data);
+                            item.SubItems.Add(displayValue);
+                            list.Items.Add(item);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void hardwarePart_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string key = "";
+            switch (hardwarePart.SelectedItem.ToString())
+            {
+                case "Процессор":
+                    key = "Win32_Processor";
+                    break;
+                case "Видеокарта":
+                    key = "Win32_VideoController";
+                    break;
+                case "Сокет":
+                    key = "Win32_IDEController";
+                    break;
+                case "Батарея":
+                    key = "Win32_Battery";
+                    break;
+                case "Биос":
+                    key = "Win32_BIOS";
+                    break;
+                case "Оперативная память":
+                    key = "Win32_PhysicalMemory";
+                    break;
+                case "Кэш":
+                    key = "Win32_CacheMemory";
+                    break;
+                case "USB":
+                    key = "Win32_USBController";
+                    break;
+                case "Диск":
+                    key = "Win32_DiskDrive";
+                    break;
+                case "Логические диски":
+                    key = "Win32_LogicalDisk";
+                    break;
+                case "Клавиатура":
+                    key = "Win32_Keyboard";
+                    break;
+                case "Сеть":
+                    key = "Win32_NetworkAdapter";
+                    break;
+                case "Пользователи":
+                    key = "Win32_Account";
+                    break;
+                case "Динамическая информация":
+                    timer.Start();
+                    return;
+                default:
+                    key = "";
+                    break;
+            }
+            timer.Stop();
+            if (key != "")
+                GetHardWareInfo(key, hardwarePartInfo);
+        }
+
+        private string ConvertDataToString(PropertyData data)
+        {
+            switch (data.Value.GetType().ToString())
+            {
+                case "System.String[]":
+                    return string.Join(" ", (string[])data.Value);
+                case "System.UInt16[]":
+                    return string.Join(" ", Array.ConvertAll((ushort[])data.Value, Convert.ToString));
+                default:
+                    return data.Value.ToString();
+            }
+        }
+    }
+}
